8851c30608bd3b3158b9dd2a1a053d6991719963
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-21 19:24:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,68 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = (props) => {
+  const [index, setIndex] = React.useState(0);
+  const [nextButton, setNextButton] = React.useState(false);
+  const [prevButton, setPrevButton] = React.useState(true);
+
+  const onNext = (el) => {
+    setIndex(index + 1);
+    if (index === props.slides.length - 2) {
+      setNextButton(true);
+      return;
+    }
+    setNextButton(false);
+    setPrevButton(false);
+  };
+
+  const onPrev = (el) => {
+    if (index === 0) {
+      setPrevButton(true);
+      return;
+    }
+    setIndex(index - 1);
+    setPrevButton(false);
+    setPrevButton(false);
+  };
+
+  const onRestart = (el) => {
+    setIndex(0);
+    setNextButton(false);
+    setPrevButton(true);
+  };
+
   return (
-    <></>
-  )
-}
+    <>
+      <h1 data-testid="title">{props.slides[index].title}</h1>
+      <p data-testid="text">{props.slides[index].text}</p>
 
+      <button
+        data-testid="button-prev"
+        disabled={prevButton}
+        onClick={(el) => onPrev(el)}
+      >
+        Prev
+      </button>
+
+      <button
+        data-testid="button-next"
+        disabled={nextButton}
+        onClick={(el) => onNext(el)}
+      >
+        Next
+      </button>
+
+      <button
+        data-testid="button-restart"
+        onClick={(el) => {
+          onRestart(el);
+        }}
+      >
+        Restart"
+      </button>
+    </>
+  );
+};
 
 export default App;
